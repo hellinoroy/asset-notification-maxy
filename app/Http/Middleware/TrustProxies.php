bf218442f38b147d994484add8edacf93d7a863b
hellinoroy <marchellinoryo@gmail.com>
2025-07-07 19:50:58 +0700

    update TrustProxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Middleware\TrustProxies as Middleware;
 class TrustProxies extends Middleware
 {
     protected $proxies = '*';
-
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    
+    protected $headers = Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PROTO;
 }
