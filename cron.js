c5255ab53ba4cddadc508b2045af7d374cc48e58
hellinoroy <167958930+hellinoroy@users.noreply.github.com>
2025-07-10 09:53:32 +0700

    Update cron.js

--- cron.js
+++ cron.js
@@ -1,6 +1,7 @@
 import cron from 'node-cron';
 import shell from 'shelljs';
 
+   shell.exec('php artisan queue:listen>> /dev/null 2>&1');
 // Schedule tasks to be run on the server.
 cron.schedule('* * * * *', function() {
   console.log('Running cronjobs');
