e29035b821404f1e3c699eaa6c42dc2301278e0a
hellinoroy <marchellinoryo@gmail.com>
2025-07-10 08:51:42 +0700

    package audit & cron job

---
+++ cron.ts
@@ -0,0 +1,8 @@
+const cron = require('node-cron');
+const shell = require('shelljs');
+
+// Schedule tasks to be run on the server.
+cron.schedule('* * * * *', function() {
+  console.log('Running cronjobs');
+  shell.exec('php artisan schedule:run >> /dev/null 2>&1');
+});
