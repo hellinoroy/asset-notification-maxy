d1a10fb254d4c3f8e5d0ee92c5225002cc23a2fc
hellinoroy <marchellinoryo@gmail.com>
2025-07-10 15:34:10 +0700

    integrasi register dan login

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -1,40 +1,27 @@
-// import { Head, useForm } from '@inertiajs/react';
-// import { LoaderCircle } from 'lucide-react';
-// import { FormEventHandler } from 'react';
+import InputError from '@/components/input-error';
+import { type SharedData } from '@/types';
+import { Link, useForm, usePage } from '@inertiajs/react';
+import { FormEventHandler, useState } from 'react';
 
-// import InputError from '@/components/input-error';
-// import TextLink from '@/components/text-link';
-// import { Button } from '@/components/ui/button';
-// import { Checkbox } from '@/components/ui/checkbox';
-// import { Input } from '@/components/ui/input';
-// import { Label } from '@/components/ui/label';
-// import AuthLayout from '@/layouts/auth-layout';
+import { Checkbox } from '@/components/ui/checkbox';
+import AuthFormContainer from '../../components/AuthFormContainer';
+import InputField from '../../components/InputField';
+import PrimaryButton from '../../components/PrimaryButton';
 
-// type LoginForm = {
-//     email: string;
-//     password: string;
-//     remember: boolean;
-// };
+import ilustrasiLogin from '../../../../public/login-illustration.jpg';
 
-// interface LoginProps {
-//     status?: string;
-//     canResetPassword: boolean;
-// }
+type LoginForm = {
+    email: string;
+    password: string;
+    remember: boolean;
+};
+
+interface LoginProps {
+    status?: string;
+    canResetPassword: boolean;
+}
 
 // export default function Login({ status, canResetPassword }: LoginProps) {
-//     const { data, setData, post, processing, errors, reset } = useForm<Required<LoginForm>>({
-//         email: '',
-//         password: '',
-//         remember: false,
-//     });
-
-//     const submit: FormEventHandler = (e) => {
-//         e.preventDefault();
-//         post(route('login'), {
-//             onFinish: () => reset('password'),
-//         });
-//     };
-
 //     return (
 //         <AuthLayout title="Log in to your account" description="Enter your email and password below to log in">
 //             <Head title="Log in" />
@@ -43,18 +30,7 @@
 //                 <div className="grid gap-6">
 //                     <div className="grid gap-2">
 //                         <Label htmlFor="email">Email address</Label>
-//                         <Input
-//                             id="email"
-//                             type="email"
-//                             required
-//                             autoFocus
-//                             tabIndex={1}
-//                             autoComplete="email"
-//                             value={data.email}
-//                             onChange={(e) => setData('email', e.target.value)}
-//                             placeholder="email@example.com"
-//                         />
-//                         <InputError message={errors.email} />
+
 //                     </div>
 
 //                     <div className="grid gap-2">
@@ -109,124 +85,77 @@
 //     );
 // }
 
-import { type SharedData } from '@/types';
-import { Link, usePage } from '@inertiajs/react';
-import { useState } from 'react';
-
-import AuthFormContainer from '../../components/AuthFormContainer';
-import InputField from '../../components/InputField';
-import PrimaryButton from '../../components/PrimaryButton';
-
-import ilustrasiLogin from '../../../../public/login-illustration.jpg';
-
-export default function Login() {
+export default function Login({ status, canResetPassword }: LoginProps) {
     const { auth } = usePage<SharedData>().props;
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [rememberMe, setRememberMe] = useState(false);
 
-    const handleLogin = (e: any) => {
+    const { data, setData, post, processing, errors, reset } = useForm<Required<LoginForm>>({
+        email: '',
+        password: '',
+        remember: false,
+    });
+
+    const submit: FormEventHandler = (e: any) => {
         e.preventDefault();
-        // Logika login di sini
-
-        // Validasi: Pastikan email dan password tidak kosong
-        if (!email || !password) {
-            alert('Mohon masukkan email dan kata sandi Anda.');
-            return; // Hentikan proses jika ada input yang kosong
-        }
-
-        console.log('Email:', email);
-        console.log('Password:', password);
-        console.log('Remember Me:', rememberMe);
-
-        // Simulasi login berhasil
-        // Dalam aplikasi nyata, Anda akan mengirim data ke server
-        // dan menunggu respons sukses sebelum melakukan navigasi.
-        const isLoginSuccessful = true; // Ganti dengan logika autentikasi sebenarnya
-
-        if (isLoginSuccessful) {
-            alert('Login berhasil! Mengarahkan ke halaman jadwal...');
-            //   navigate('/admin/schedule'); // Arahkan ke rute jadwal admin
-        } else {
-            alert('Login gagal. Periksa kembali email dan kata sandi Anda.');
-            // Anda bisa menampilkan pesan error yang lebih spesifik di UI
-        }
+        post(route('login'), {
+            onFinish: () => reset('password'),
+        });
     };
+
+
 
     return (
         <>
-            {/* <Link
-                href={route('dashboard')}
-                className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
-            >
-                Dashboard
-            </Link> */}
-            {/* <header className="mb-6 w-full max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-4xl">
-                <nav className="flex items-center justify-end gap-4">
-                    {auth.user ? (
-                        <Link
-                            href={route('dashboard')}
-                            className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
-                        >
-                            Dashboard
-                        </Link>
-                    ) : (
-                        <>
-                            <Link
-                                href={route('login')}
-                                className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
-                            >
-                                Log in
-                            </Link>
-                            <Link
-                                href={route('register')}
-                                className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
-                            >
-                                Register
-                            </Link>
-                        </>
-                    )}
-                </nav>
-            </header> */}
-
             <AuthFormContainer imageUrl={ilustrasiLogin}>
                 {/* Menggunakan ukuran teks text-xl yang terakhir kita sepakati */}
                 <h2 className="mb-2 text-xl font-bold text-gray-800">Halo, Selamat Datang kembali ! 👋</h2>
                 <p className="mb-8 text-gray-600">Silakan masuk ke akun Anda</p>
 
-                <form onSubmit={handleLogin} className="space-y-6">
+                <form onSubmit={submit} className="space-y-6">
                     <InputField
                         id="email"
                         type="email"
+                        required
+                        autoFocus
+                        tabIndex={1}
+                        autoComplete="email"
+                        value={data.email}
+                        onChange={(e: any) => setData('email', e.target.value)}
                         placeholder="Masukkan email kamu kesini"
-                        value={email}
-                        onChange={(e: any) => setEmail(e.target.value)}
                     />
+                    <InputError message={errors.email} />
+
                     <InputField
                         id="password"
                         type="password"
+                        required
+                        tabIndex={2}
+                        autoComplete="current-password"
+                        value={data.password}
+                        onChange={(e: any) => setData('password', e.target.value)}
                         placeholder="Kata sandi kamu"
-                        value={password}
-                        onChange={(e: any) => setPassword(e.target.value)}
-                        showPasswordToggle // Aktifkan toggle lihat/sembunyikan password
+                        showPasswordToggle
                     />
+                    <InputError message={errors.password} />
+
+                    <div className="flex items-center space-x-3"></div>
 
                     <div className="flex items-center justify-between text-sm">
                         <div className="flex items-center">
-                            <input
-                                id="remember-me"
-                                type="checkbox"
-                                checked={rememberMe}
-                                onChange={(e) => setRememberMe(e.target.checked)}
+                            <Checkbox
+                                id="remember "
+                                name="remember"
+                                checked={data.remember}
+                                onClick={() => setData('remember', !data.remember)}
+                                tabIndex={3}
                                 className="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500"
                             />
                             <label htmlFor="remember-me" className="ml-2 text-gray-700">
                                 Remember Me
                             </label>
                         </div>
-                        {/* <Link to="/forgot-password" className="text-blue-600 hover:underline">
-            Lupa kata sandi?
-          </Link> */}
                     </div>
 
                     <PrimaryButton type="submit">Masuk</PrimaryButton>
--- resources/js/pages/auth/verify-email.tsx
+++ resources/js/pages/auth/verify-email.tsx
@@ -1,11 +1,9 @@
 // Components
-import { Head, useForm } from '@inertiajs/react';
+import { Button } from '@/components/ui/button';
+import { useForm } from '@inertiajs/react';
 import { LoaderCircle } from 'lucide-react';
 import { FormEventHandler } from 'react';
-
 import TextLink from '@/components/text-link';
-import { Button } from '@/components/ui/button';
-import AuthLayout from '@/layouts/auth-layout';
 
 export default function VerifyEmail({ status }: { status?: string }) {
     const { post, processing } = useForm({});
@@ -17,9 +15,7 @@ export default function VerifyEmail({ status }: { status?: string }) {
     };
 
     return (
-        <AuthLayout title="Verify email" description="Please verify your email address by clicking on the link we just emailed to you.">
-            <Head title="Email verification" />
-
+        <>
             {status === 'verification-link-sent' && (
                 <div className="mb-4 text-center text-sm font-medium text-green-600">
                     A new verification link has been sent to the email address you provided during registration.
@@ -36,6 +32,6 @@ export default function VerifyEmail({ status }: { status?: string }) {
                     Log out
                 </TextLink>
             </form>
-        </AuthLayout>
+        </>
     );
 }
